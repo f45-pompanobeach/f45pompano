2407f9b302bb05918a12c2af74613951eb0f1aba
f45-pompanobeach <pompanobeach@f45training.com>
2026-05-20 11:46:49 -0400

    Update build.js

--- build.js
+++ build.js
@@ -7,17 +7,41 @@ const genericTemplate = fs.readFileSync("template-generic.html", "utf8");
 const dataDir = "data";
 const distDir = "dist";
 
+// NEW
+const shared = JSON.parse(
+  fs.readFileSync(path.join(dataDir, "shared.json"), "utf8")
+);
+
 fs.rmSync(distDir, { recursive: true, force: true });
 fs.mkdirSync(distDir, { recursive: true });
 
 // Default homepage
-fs.writeFileSync(path.join(distDir, "index.html"), genericTemplate);
+let genericHtml = genericTemplate;
+
+// NEW
+for (const [key, value] of Object.entries(shared)) {
+  genericHtml = genericHtml.replaceAll(`{{${key}}}`, value);
+}
+
+fs.writeFileSync(path.join(distDir, "index.html"), genericHtml);
 
 // Partner pages
 for (const file of fs.readdirSync(dataDir)) {
+
+  // NEW
+  if (file === "shared.json") continue;
+
   if (!file.endsWith(".json")) continue;
 
-  const data = JSON.parse(fs.readFileSync(path.join(dataDir, file), "utf8"));
+  const partner = JSON.parse(
+    fs.readFileSync(path.join(dataDir, file), "utf8")
+  );
+
+  // NEW
+  const data = {
+    ...shared,
+    ...partner
+  };
 
   let html = partnerTemplate;
 
@@ -26,5 +50,6 @@ for (const file of fs.readdirSync(dataDir)) {
   }
 
   fs.writeFileSync(path.join(distDir, `${data.slug}.html`), html);
+
   console.log(`Generated ${data.slug}.html`);
 }
