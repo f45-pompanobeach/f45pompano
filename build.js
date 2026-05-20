3ce561c26c533138c91b0933698af0d0edc33c84
f45-pompanobeach <pompanobeach@f45training.com>
2026-05-20 10:22:48 -0400

    Update build.js

--- build.js
+++ build.js
@@ -1,7 +1,15 @@
 const fs = require("fs");
 const path = require("path");
 
-const template = fs.readFileSync("template.html", "utf8");
+const partnerTemplate = fs.readFileSync(
+  "template-partner.html",
+  "utf8"
+);
+
+const genericTemplate = fs.readFileSync(
+  "template-generic.html",
+  "utf8"
+);
 
 const dataDir = "data";
 const distDir = "dist";
@@ -9,22 +17,31 @@ const distDir = "dist";
 fs.rmSync(distDir, { recursive: true, force: true });
 fs.mkdirSync(distDir, { recursive: true });
 
+// GENERIC HOMEPAGE
+fs.writeFileSync(
+  path.join(distDir, "index.html"),
+  genericTemplate
+);
+
+// PARTNER PAGES
 for (const file of fs.readdirSync(dataDir)) {
+
   if (!file.endsWith(".json")) continue;
 
-  const data = JSON.parse(fs.readFileSync(path.join(dataDir, file), "utf8"));
+  const data = JSON.parse(
+    fs.readFileSync(path.join(dataDir, file), "utf8")
+  );
 
-  let html = template;
+  let html = partnerTemplate;
 
   for (const [key, value] of Object.entries(data)) {
     html = html.replaceAll(`{{${key}}}`, value);
   }
 
-  fs.writeFileSync(path.join(distDir, `${data.slug}.html`), html);
-
-  if (data.slug === "foxtail") {
-    fs.writeFileSync(path.join(distDir, "index.html"), html);
-  }
+  fs.writeFileSync(
+    path.join(distDir, `${data.slug}.html`),
+    html
+  );
 
   console.log(`Generated ${data.slug}.html`);
 }
