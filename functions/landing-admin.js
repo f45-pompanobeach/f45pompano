8042f3794468a71874431f86834ad4eb80578fe9
f45-pompanobeach <285754725+f45-pompanobeach@users.noreply.github.com>
2026-09-18 13:30:36 -0400

    Remove unnecessary landing admin route function

--- functions/landing-admin.js
+++
@@ -1,6 +0,0 @@
-export function onRequestGet({request}){
-  const from=new URL(request.url);
-  const to=new URL('/landing/admin/',from.origin);
-  return Response.redirect(to.toString(),302);
-}
-export function onRequest(){return new Response('Method not allowed',{status:405})}
