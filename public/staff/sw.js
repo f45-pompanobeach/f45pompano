9e37a8cb4dfdb706143180cb4317f6e0b91805d1
f45-pompanobeach <285754725+f45-pompanobeach@users.noreply.github.com>
2026-09-20 14:44:16 -0400

    PWA: bump cache for v0.1.6

--- public/staff/sw.js
+++ public/staff/sw.js
@@ -1,4 +1,4 @@
-const CACHE='f45-leads-trials-staff-v5';
+const CACHE='f45-leads-trials-staff-v6';
 const CORE=['/staff/','/staff/access/','/staff/manifest.webmanifest','/staff/icon-leads-trials.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE&&k.startsWith('f45-leads-trials-')).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
