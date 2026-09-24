505dd80a978ccfe86bb18e5425a68860a7ec3410
f45-pompanobeach <285754725+f45-pompanobeach@users.noreply.github.com>
2026-09-24 14:45:27 -0400

    PWA: refresh Leads/Trials v0.1.12

--- public/leads-trials/sw.js
+++ public/leads-trials/sw.js
@@ -1,4 +1,4 @@
-const CACHE='f45-leads-trials-v12';
+const CACHE='f45-leads-trials-v13';
 const CORE=['/leads-trials/','/leads-trials/access/','/leads-trials/manifest.webmanifest','/leads-trials/icon-leads-trials.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE&&k.startsWith('f45-leads-trials-')).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
